d8640164e83681e49f41f87cfb6e364ca5610f2d
shenzhihua <shenzhihua@zuowenzhitiao.com>
2025-07-08 19:06:27 +0800

    优化工具链的使用；

--- scripts/package-toolchain.sh
+++ scripts/package-toolchain.sh
@@ -4,19 +4,33 @@ set -e
 
 # 环境变量设置
 OPENWRT_PATH="${OPENWRT_PATH:-$GITHUB_WORKSPACE/openwrt}"
-CACHE_NAME="${CACHE_NAME:-$FIRMWARE_TYPE-toolchain}"
 
 # 打包Toolchain
 if [[ $REBUILD_TOOLCHAIN = 'true' ]]; then
     cd $OPENWRT_PATH
     sed -i 's/ $(tool.*\/stamp-compile)//' Makefile
-    [ -d ".ccache" ] && (ccache=".ccache"; ls -alh .ccache)
-    du -h --max-depth=1 ./staging_dir
-    du -h --max-depth=1 ./ --exclude=staging_dir
+    [ -d ".ccache" ] && ccache=".ccache"
     tar -I zstdmt -cf $GITHUB_WORKSPACE/output/$CACHE_NAME.tzst staging_dir/host* staging_dir/tool* $ccache
-    ls -lh $GITHUB_WORKSPACE/output
     [ -e $GITHUB_WORKSPACE/output/$CACHE_NAME.tzst ] || exit 1
     exit 0
 fi
 
 [ -d $GITHUB_WORKSPACE/output ] || mkdir $GITHUB_WORKSPACE/output
+cd $OPENWRT_PATH
+
+# 下载并部署Toolchain
+if [[ $TOOLCHAIN = 'true' ]]; then
+    cache_url=$(curl -sL api.github.com/repos/$GITHUB_REPOSITORY/releases | awk -F '"' '/download_url/{print $4}' | grep $CACHE_NAME)
+
+    if [[ $cache_url ]]; then
+        wget -qc -t=3 $cache_url
+        if [ -e *.tzst ]; then
+            tar -I unzstd -xf *.tzst || tar -xf *.tzst
+            sed -i 's/ $(tool.*\/stamp-compile)//' Makefile
+        fi
+    else
+        echo "REBUILD_TOOLCHAIN=true" >> $GITHUB_ENV
+    fi
+else
+    echo "REBUILD_TOOLCHAIN=true" >> $GITHUB_ENV
+fi
