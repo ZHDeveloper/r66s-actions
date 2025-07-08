1c8a8b266da5142730cd7b37327a3119a5bd96cf
shenzhihua <shenzhihua@zuowenzhitiao.com>
2025-07-08 19:19:15 +0800

    优化脚本；

--- scripts/package-toolchain.sh
+++ scripts/package-toolchain.sh
@@ -5,6 +5,12 @@ set -e
 # 环境变量设置
 OPENWRT_PATH="${OPENWRT_PATH:-$GITHUB_WORKSPACE/openwrt}"
 
+# 生成工具链缓存文件名
+cd $OPENWRT_PATH
+TOOLS_HASH=$(git log --pretty=tformat:"%h" -n1 tools toolchain)
+CACHE_NAME="$FIRMWARE_TYPE-toolchain-cache-$TOOLS_HASH"
+echo "CACHE_NAME=$CACHE_NAME" >> $GITHUB_ENV
+
 # 打包Toolchain
 if [[ $REBUILD_TOOLCHAIN = 'true' ]]; then
     cd $OPENWRT_PATH
@@ -16,7 +22,6 @@ if [[ $REBUILD_TOOLCHAIN = 'true' ]]; then
 fi
 
 [ -d $GITHUB_WORKSPACE/output ] || mkdir $GITHUB_WORKSPACE/output
-cd $OPENWRT_PATH
 
 # 下载并部署Toolchain
 cache_url=$(curl -sL api.github.com/repos/$GITHUB_REPOSITORY/releases | awk -F '"' '/download_url/{print $4}' | grep $CACHE_NAME)
