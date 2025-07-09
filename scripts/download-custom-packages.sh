b6cb3ef2c7c6f4110b2270225822209c7c65452f
shenzhihua <shenzhihua@zuowenzhitiao.com>
2025-07-09 16:21:22 +0800

    使用默认的 argon；

--- scripts/download-custom-packages.sh
+++ scripts/download-custom-packages.sh
@@ -24,7 +24,6 @@ clone_package() {
 mkdir -p package
 
 # Clone common packages
-clone_package "https://github.com/jerrykuku/luci-theme-argon" "package/luci-theme-argon"
 clone_package "https://github.com/fw876/helloworld" "package/luci-app-ssr-plus"
 clone_package "https://github.com/xiaorouji/openwrt-passwall" "package/luci-app-passwall"
 clone_package "https://github.com/linkease/istore" "package/luci-app-store"
