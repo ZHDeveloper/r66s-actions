e780e0df63ea5a420ca8853d5ac71d44757d38bb
shenzhihua <shenzhihua@zuowenzhitiao.com>
2025-07-11 11:48:24 +0800

    修改下载脚本；

--- scripts/download-cores.sh
+++ scripts/download-cores.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
 set -e
 
-download_file() {
-    local url="$1" output="$2"
-    wget -q -O "$output" "$url" 2>/dev/null || return 1
-}
-
 # Download AdGuard Home
-agh_url=$(curl -s https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | \
-          grep "browser_download_url.*linux_arm64.tar.gz" | cut -d '"' -f 4)
-
-if [ -n "$agh_url" ] && download_file "$agh_url" "/tmp/agh.tar.gz"; then
-    tar -xzf /tmp/agh.tar.gz -C /tmp/ && \
-    mkdir -p files/usr/bin/AdGuardHome && \
-    cp /tmp/AdGuardHome/AdGuardHome files/usr/bin/AdGuardHome/ && \
-    chmod +x files/usr/bin/AdGuardHome/AdGuardHome
-    rm -rf /tmp/agh.tar.gz /tmp/AdGuardHome
-fi
+[ -d files/usr/bin/AdGuardHome ] || mkdir -p files/usr/bin/AdGuardHome
+AGH_CORE="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_arm64.tar.gz"
+wget -qO- $AGH_CORE | tar xOz > files/usr/bin/AdGuardHome/AdGuardHome
+chmod +x files/usr/bin/AdGuardHome/AdGuardHome
 
 # Download OpenClash core and geo files
-mkdir -p files/etc/openclash/core files/etc/openclash
+[ -d files/etc/openclash/core ] || mkdir -p files/etc/openclash/core
 
-# OpenClash core
-if download_file "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz" "/tmp/clash.tar.gz"; then
-    tar -xzf /tmp/clash.tar.gz -C /tmp/ && \
-    cp /tmp/clash files/etc/openclash/core/clash_meta && \
-    chmod +x files/etc/openclash/core/clash_meta
-    rm -rf /tmp/clash.tar.gz /tmp/clash
-fi
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+GEOIP_URL="https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat"
+GEOSITE_URL="https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat"
+COUNTRY_URL="https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb"
 
-# Geo files
-download_file "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" "files/etc/openclash/GeoIP.dat" &
-download_file "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" "files/etc/openclash/GeoSite.dat" &
-download_file "https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb" "files/etc/openclash/Country.mmdb" &
-wait
+wget -qO- $CLASH_META_URL | tar xOz > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+wget -qO- $COUNTRY_URL > files/etc/openclash/Country.mmdb
+
+chmod +x files/etc/openclash/core/clash_meta
