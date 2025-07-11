d849d83ee56687dc3181d619ef66efc8fecb79e9
shenzhihua <shenzhihua@zuowenzhitiao.com>
2025-07-11 12:11:46 +0800

    修改脚本；

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -4,14 +4,10 @@ set -e
 # Set default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Set default IP (192.168.100.1)
-sed -i '/n) ipad/s/".*"/"192.168.100.1"/' package/base-files/files/bin/config_generate
+
 
 # Set default password (password)
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
-
-# Configure ttyd to auto-login as root
-sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # Add build timestamp
 echo "Built on $(TZ=UTC-8 date "+%Y-%m-%d %H:%M:%S")" >> package/base-files/files/etc/banner
