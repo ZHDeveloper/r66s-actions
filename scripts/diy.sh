59c4d278403ade13beca0b129c6b34fa293a456a
shenzhihua <shenzhihua@zuowenzhitiao.com>
2025-07-11 12:13:49 +0800

    修改脚本；

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -13,5 +13,8 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:
 # Configure ttyd to auto-login as root
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
+# Modify TTYD Chinese translation
+sed -i 's/\"终端\"/\"TTYD 终端\"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
+
 # Add build timestamp
 echo "Built on $(TZ=UTC-8 date "+%Y-%m-%d %H:%M:%S")" >> package/base-files/files/etc/banner
