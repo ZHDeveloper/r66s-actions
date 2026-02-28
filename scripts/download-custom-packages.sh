f7f7fb3ae87bbab7d8c374c74dc9af4800993750
ZhiHua <shenzhihua@zuowenzhitiao.com>
2026-02-28 10:14:33 +0800

    优化代码；

--- scripts/download-custom-packages.sh
+++ scripts/download-custom-packages.sh
@@ -6,8 +6,9 @@ if [ -n "$GITHUB_TOKEN" ]; then
     git config --global url."https://x-access-token:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
 fi
 
+# ── Helper functions ──────────────────────────────────────────────────────────
 
-# Sparse clone function
+# Sparse clone: clone only specified subdirectories and move them to package/
 git_sparse_clone() {
     local branch="$1" repourl="$2"
     shift 2
@@ -21,27 +22,23 @@ git_sparse_clone() {
     cd .. && rm -rf "$repodir"
 }
 
-# Clone package function
+# Clone a package repository (with optional branch)
 clone_package() {
     local url="$1" target="$2" branch="$3"
     [ -n "$branch" ] && git clone --depth=1 -b "$branch" "$url" "$target" || git clone --depth=1 "$url" "$target"
 }
 
+# ── Custom packages ───────────────────────────────────────────────────────────
+
 mkdir -p package
 
-# Clone common packages
 clone_package "https://github.com/fw876/helloworld" "package/luci-app-ssr-plus"
 clone_package "https://github.com/Openwrt-Passwall/openwrt-passwall" "package/luci-app-passwall"
-# clone_package "https://github.com/linkease/istore" "package/luci-app-store"
 
-# Clone linkease packages
 git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
-# git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-linkease
 git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
-# git_sparse_clone master https://github.com/linkease/nas-packages network/services/linkease
-# git_sparse_clone master https://github.com/linkease/nas-packages network/services/linkmount
 
-# sbwml/luci-app-mosdns
+# Remove conflicting feed packages before installing custom ones
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 rm -rf feeds/packages/net/mosdns
@@ -55,22 +52,64 @@ clone_package "https://github.com/sbwml/luci-app-mosdns" "package/mosdns" "v5"
 clone_package "https://github.com/sbwml/v2ray-geodata" "package/v2ray-geodata"
 clone_package "https://github.com/sbwml/packages_lang_golang" "feeds/packages/lang/golang"
 
-# ImmortalWrt specific packages
 if [[ "$FIRMWARE_TYPE" == "ImmortalWrt" ]]; then
-    # Clone adguardhome from coolsnowwolf/luci
     git_sparse_clone openwrt-23.05 https://github.com/coolsnowwolf/luci applications/luci-app-adguardhome
-    # Clone nikki (Mihomo transparent proxy)
-    # git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki luci-app-nikki nikki
 fi
 
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
-# Flippy firmware specific (Amlogic toolbox)
 if [[ "$CONFIG_FILE" == *"flippy"* ]]; then
     git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
-
-    # Configure amlogic package
     config_file="package/luci-app-amlogic/root/etc/config/amlogic"
     sed -i "s|option amlogic_firmware_repo.*|option amlogic_firmware_repo 'https://github.com/$GITHUB_REPOSITORY'|g" "$config_file"
     sed -i "s|option amlogic_firmware_tag.*|option amlogic_firmware_tag '$RELEASE_TAG'|g" "$config_file"
+fi
+
+# ── Download binary cores ─────────────────────────────────────────────────────
+
+# AdGuard Home
+[ -d files/usr/bin/AdGuardHome ] || mkdir -p files/usr/bin/AdGuardHome
+wget -qO- https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_arm64.tar.gz \
+    | tar xOz > files/usr/bin/AdGuardHome/AdGuardHome
+chmod +x files/usr/bin/AdGuardHome/AdGuardHome
+
+# OpenClash core and geo files
+[ -d files/etc/openclash/core ] || mkdir -p files/etc/openclash/core
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz \
+    | tar xOz > files/etc/openclash/core/clash_meta
+wget -qO- https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat    > files/etc/openclash/GeoIP.dat
+wget -qO- https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat  > files/etc/openclash/GeoSite.dat
+wget -qO- https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb   > files/etc/openclash/Country.mmdb
+chmod +x files/etc/openclash/core/clash_meta
+
+# ── DIY customizations ────────────────────────────────────────────────────────
+
+# Set default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Set default IP
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+[ -f "package/base-files/luci2/bin/config_generate" ] && \
+    sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/luci2/bin/config_generate
+
+# Set default hostname
+sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
+[ -f "package/base-files/luci2/bin/config_generate" ] && \
+    sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/luci2/bin/config_generate
+
+# Set default password
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' \
+    package/base-files/files/etc/shadow
+
+# Configure ttyd auto-login
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+sed -i 's/\"终端\"/\"TTYD 终端\"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
+
+# Add build timestamp
+echo "Built on $(TZ=UTC-8 date "+%Y-%m-%d %H:%M:%S")" >> package/base-files/files/etc/banner
+
+# Kernel version (LEDE only)
+if [[ "$FIRMWARE_TYPE" == "LEDE" ]]; then
+    sed -i "s/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.12/g" target/linux/rockchip/Makefile
+    sed -i "s/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=6.12/g" target/linux/rockchip/Makefile
 fi
