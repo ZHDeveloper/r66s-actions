81c663d08e4f9dc57f5217ecce5015dc2cd7b3d3
ZhiHua <shenzhihua@zuowenzhitiao.com>
2026-03-09 18:29:30 +0800

    更新 rust；

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -48,11 +48,15 @@ clone_package "https://github.com/sbwml/luci-app-mosdns" "package/mosdns" "v5"
 clone_package "https://github.com/sbwml/v2ray-geodata" "package/v2ray-geodata"
 clone_package "https://github.com/sbwml/packages_lang_golang" "feeds/packages/lang/golang"
 clone_package "https://github.com/fw876/helloworld" "package/luci-app-ssr-plus"
-rm -rf package/luci-app-ssr-plus/shadowsocks-rust  # Rust 依赖，GitHub runner 无法编译 LLVM
 clone_package "https://github.com/Openwrt-Passwall/openwrt-passwall" "package/luci-app-passwall"
 clone_package "https://github.com/Openwrt-Passwall/openwrt-passwall" "package/passwall-luci"
 clone_package "https://github.com/Openwrt-Passwall/openwrt-passwall-packages" "package/passwall-packages"
-rm -rf package/passwall-packages/shadowsocks-rust   # Rust 依赖，GitHub runner 无法编译 LLVM
+
+# 通过引入 coolsnowwolf (LEDE) 的最新版 Rust (1.93.1) 替换 ImmortalWrt 老旧版(1.90.0)。
+# 因为 1.93.1 在官方服务器上的预编译 LLVM 仍在，不会 404，因此可以直接下载跳过编译，不会爆内存！
+# 移除 feeds 里的旧版，然后用 git_sparse_clone 把新版提取为局部的 package/rust 进行覆盖。
+rm -rf feeds/packages/lang/rust
+git_sparse_clone master https://github.com/coolsnowwolf/packages lang/rust
 
 git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
 git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
